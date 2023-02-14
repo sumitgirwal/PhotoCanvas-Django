f6aee9004ad7abc823ef12d157bc98aedaf6f6cc
Sumit <sumitgirwals@gmail.com>
2023-02-14 19:06:20 +0530

    Added crop image feature and updated views function

--- PhotoCanvas/photoedit/urls.py
+++ PhotoCanvas/photoedit/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('crop-image', views.cropImage , name='crop_image'),
     path('upload-image', views.uploadImage, name='upload_image'),
 
+    path('upload/', views.upload_file, name='upload_file'),
+    path('temp/', views.temp, name='temp'),
+    path('temp1/', views.temp1, name='temp1'),
+
 ]
